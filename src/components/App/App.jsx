7834e0de3bddd63c578636b60c4a6d4e6139ee08
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-28 22:58:50 +0300

    refactored App.jsx using hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,89 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { animateScroll } from 'react-scroll';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { fetchImages } from 'services/api';
-import {Searchbar} from 'components/Searchbar/Searchbar';
+import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    queryPage: 1,
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [queryPage, setQueryPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [totalImages, setTotalImages] = useState(null);
+  // const [error, setError] = useState(false);
 
-    loading: false,
-    images: [],
-    totalImages: 0,
-
-    error: false,
-  };
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.queryPage !== this.state.queryPage) {
-      this.getImages();
-    }
-  }
-
-  getImages = async () => {
-    const { searchQuery, queryPage, images } = this.state;
-    this.setState({ loading: true });
-
-    if (searchQuery.trim() === '') {
-      toast.info('You cannot search by empty field, try again.');
-      this.setState({ loading: false });
-
+  useEffect(() => {
+    if (!searchQuery) {
       return;
-    } else {
-      try {
-        const { totalHits, hits } = await fetchImages(searchQuery, queryPage);
-        console.log(totalHits, hits);
-
-        if (hits.length === 0) {
-          toast.warn(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-          this.setState({ images: [], loading: false });
-        } else {
-          this.setState({
-            images: queryPage === 1 ? hits : [...images, ...hits],
-            totalImages: totalHits,
-            loading: false,
-          });
-        }
-      } catch (error) {
-        this.setState({ error: true });
-        toast.error('Sorry, something go wrong! Try again!');
-      }
     }
-  };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ queryPage: 1, searchQuery, loading: true }, this.getImages);
+    const getImages = async () => {
+      setLoading(true);
+
+      if (searchQuery.trim() === '') {
+        toast.info('You cannot search by empty field, try again.');
+        setLoading(false);
+
+        return;
+      } else {
+        try {
+          const { totalHits, hits } = await fetchImages(searchQuery, queryPage);
+          // console.log(totalHits, hits);
+
+          if (hits.length === 0) {
+            toast.warn(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+            setImages([]);
+            setLoading(false);
+          } else {
+            if (queryPage === 1) {
+              setImages(hits);
+            } else {
+              setImages(prevImages => [...prevImages, ...hits]);
+            }
+
+            setTotalImages(totalHits);
+            setLoading(false);
+          }
+        } catch (error) {
+          // setError(true);
+          toast.error('Sorry, something go wrong! Try again!');
+        }
+      }
+    };
+
+    getImages();
+  }, [searchQuery, queryPage]);
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setQueryPage(1);
+    setLoading(true);
+
     animateScroll.scrollToTop({ duration: 500, smooth: 'easeInOutQuart' });
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ queryPage: prevState.queryPage + 1 }));
+  const handleLoadMore = () => {
+    setQueryPage(prevState => prevState + 1);
     animateScroll.scrollToBottom({ duration: 500, smooth: 'easeInOutQuart' });
   };
 
-  render() {
-    const { loading, images, totalImages } = this.state;
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ToastContainer autoClose={3000} />
 
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ToastContainer autoClose={3000} />
-
-        <ImageGallery images={images} />
-        {loading && <Loader />}
-        {!loading && images.length > 0 && images.length < totalImages && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-      </>
-    );
-  }
-}
+      <ImageGallery images={images} />
+      {loading && <Loader />}
+      {!loading && images.length > 0 && images.length < totalImages && (
+        <Button onClick={handleLoadMore} />
+      )}
+    </>
+  );
+};
