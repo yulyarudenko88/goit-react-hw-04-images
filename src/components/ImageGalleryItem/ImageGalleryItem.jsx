ce9756c1d87e1da83389df386064d87053e86322
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-28 21:39:17 +0300

    refactored ImageGalleryItem.jsx using hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,35 +1,27 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'components/Modal/Modal';
 import { ImageItem, Image, ImageLarge } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+export const ImageGalleryItem = ({ src, largeImageURL, alt }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
   };
 
-  toggleModal = () => {
-    this.setState(prevState => ({ showModal: !prevState.showModal }));
-    // console.log('modal', this.state);
-  };
+  return (
+    <ImageItem>
+      <Image src={src} alt={alt} onClick={toggleModal} />
 
-  render() {
-    const { src, largeImageURL, alt } = this.props;
-    const { showModal } = this.state;
-
-    return (
-      <ImageItem>
-        <Image src={src} alt={alt} onClick={this.toggleModal} />
-
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <ImageLarge src={largeImageURL} alt={alt} />
-          </Modal>
-        )}
-      </ImageItem>
-    );
-  }
-}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <ImageLarge src={largeImageURL} alt={alt} />
+        </Modal>
+      )}
+    </ImageItem>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   src: PropTypes.string.isRequired,
