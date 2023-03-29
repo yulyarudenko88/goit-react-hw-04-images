e009fa7921b8d1b565f833eaec4530dd07d93349
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-29 14:40:30 +0300

    deleted commits

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -40,49 +40,6 @@ export const Searchbar = ({ onSubmit }) => {
     </Header>
   );
 };
-// export default class Searchar extends Component {
-//   state = {
-//     searchWord: '',
-//   };
-
-// handleChange = e => {
-//   const { value } = e.target;
-//   this.setState({ searchWord: value.toLowerCase() });
-// };
-
-// handleSubmit = e => {
-//   e.preventDefault();
-//   this.props.onSubmit(this.state.searchWord);
-//   this.reset();
-// };
-
-//   reset = () => {
-//     this.setState({ searchWord: '' });
-//   };
-
-//   render() {
-//     const { searchWord } = this.state;
-
-// return (
-//   <Header>
-//     <SearchForm onSubmit={this.handleSubmit}>
-//       <SearchButton type="submit">
-//         <VscSearch size={20} />
-//       </SearchButton>
-
-//       <SearchFormInput
-//         type="text"
-//         autocomplete="off"
-//         autoFocus
-//         placeholder="Search images and photos"
-//         value={searchWord}
-//         onChange={this.handleChange}
-//       />
-//     </SearchForm>
-//   </Header>
-// );
-//   }
-// }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
